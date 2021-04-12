e321ca643aeeb49e9f1056b8a161b10d153d0522
Denys Hutsal <denyshutsal@gmail.com>
2021-04-12 21:55:49 +0300

    added layout.vue

---
+++ src/components/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <main class="site-main">
+    <slot />
+  </main>
+  <Footer />
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
