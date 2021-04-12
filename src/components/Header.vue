8df04224bc73c7ab7a0539e0a4a8c1b285fb67ec
Denys Hutsal <denyshutsal@gmail.com>
2021-04-12 20:52:32 +0300

    added header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="site-header">
+    <img alt="site-header__logo" src="../assets/site_logo.png" width="32" />
+    <span class="site-header__text">News Fetcher</span>
+  </header>
+</template>
