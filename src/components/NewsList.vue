af619b90eeb723886055c0c7b918f1d732b176e2
Denys Hutsal <denyshutsal@gmail.com>
2021-04-13 09:13:39 +0300

    added newslist.vue

---
+++ src/components/NewsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="news-list">
+    <NewsCard v-for="(post, index) in posts" :key="index" :post="post" />
+  </div>
+</template>
+
+<script>
+import NewsCard from "./NewsCard.vue";
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    NewsCard,
+  },
+};
+</script>
