d716e801be37d43c524d7c8bc0ba2a3641b831ab
Denys Hutsal <denyshutsal@gmail.com>
2021-04-13 10:17:12 +0300

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <Layout>
-    <p>Main content goes here</p>
+    <h1 class="site-main__title">
+      News Section: <span class="site-main__span">{{ section }}</span>
+    </h1>
+    <NewsFilter v-model="section" />
+    <NewsList :posts="posts" />
   </Layout>
 </template>
 
 <script>
 import Layout from "./components/Layout.vue";
+import NewsFilter from "./components/NewsFilter.vue";
+import NewsList from "./components/NewsList.vue";
+
+import data from "./posts.json";
 
 export default {
   components: {
     Layout,
+    NewsFilter,
+    NewsList,
+  },
+  data() {
+    return {
+      section: "home",
+      posts: data.posts,
+    };
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
