cfe3cc16ef02762360043c8dceb62ab635b54122
Denys Hutsal <denyshutsal@gmail.com>
2021-04-13 17:10:01 +0300

    added styles to layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -17,3 +17,29 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.site-main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 30px 10px;
+
+  &__title {
+    font-size: 1.8rem;
+    line-height: 1.4;
+    margin-top: 0;
+    margin-bottom: 1rem;
+    color: #bdbdbd;
+  }
+
+  &__span {
+    text-transform: capitalize;
+    color: #546e7a;
+  }
+}
+</style>
