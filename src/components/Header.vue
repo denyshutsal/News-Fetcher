dbd33717db2a91ba86c3e9580acb44049e6e3510
Denys Hutsal <denyshutsal@gmail.com>
2021-04-13 17:09:42 +0300

    added styles to header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,30 @@
 <template>
   <header class="site-header">
-    <img alt="site-header__logo" src="../assets/site_logo.png" width="32" />
+    <img
+      class="site-header__logo"
+      src="../assets/site_logo.png"
+      alt="site-header__logo"
+      width="32"
+    />
     <span class="site-header__text">News Fetcher</span>
   </header>
 </template>
+
+<style lang="scss">
+.site-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: wheat;
+
+  &__logo {
+    width: 32px;
+    margin: 0.5rem;
+  }
+
+  &__text {
+    font-size: 1.2rem;
+  }
+}
+</style>
