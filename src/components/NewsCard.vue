ecd7b563688c4c807ea6c1298d67b4adb7264108
Denys Hutsal <denyshutsal@gmail.com>
2021-04-14 14:11:40 +0300

    final ver of newscardd.vue

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -9,13 +9,7 @@
       >
         {{ post.title }}
       </a>
-      <img
-        class="news-card__img"
-        :src="post.thumbnail"
-        :alt="post.caption"
-        height="140"
-        width="210"
-      />
+      <img class="news-card__img" :src="post.thumbnail" :alt="post.caption" />
       <p class="news-card__text">
         {{ post.abstract }}
       </p>
@@ -46,3 +40,57 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+%fontStyles {
+  font-size: 0.9rem;
+  font-weight: 700;
+  margin: 0;
+}
+
+.news-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1rem;
+  text-align: left;
+  background-color: #fafafa;
+  border-radius: 5px;
+  box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%);
+
+  &__header {
+    margin-bottom: 1rem;
+  }
+
+  &__title {
+    display: inline-block;
+    margin-bottom: 1rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    line-height: 1.3;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  &__img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin-bottom: 1rem;
+  }
+
+  &__text {
+    margin: 0;
+    line-height: 1.5;
+  }
+
+  &__author,
+  &__date {
+    @extend %fontStyles;
+  }
+
+  &__author {
+    margin-bottom: 0.5rem;
+  }
+}
+</style>
