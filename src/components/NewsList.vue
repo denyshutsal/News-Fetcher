c145eb28d8f2dd15e90769dca98387449ea7d05f
Denys Hutsal <denyshutsal@gmail.com>
2021-04-14 14:12:11 +0300

    final ver of newslist.vue

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -18,3 +18,25 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.news-list {
+  display: grid;
+  justify-items: center;
+  gap: 1.5rem;
+  grid-template-columns: 18rem;
+  margin-top: 3rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 18rem);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 18rem);
+  }
+
+  @media (min-width: 1536px) {
+    grid-template-columns: repeat(4, 18rem);
+  }
+}
+</style>
