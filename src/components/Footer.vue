4fc40b42f38843270a0157ac5be111a4ce0a2f6e
Denys Hutsal <denyshutsal@gmail.com>
2021-04-14 14:11:06 +0300

    final ver of footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,36 @@
 <template>
   <footer class="site-footer">
-    <p class="site-footer__text">Copyright (c) 2021 Heisenberg</p>
+    <p class="site-footer__text">
+      © Copyright NewsFetcher {{ currentYear() }}
+      <a
+        class="site-footer__link"
+        href="https://i.pinimg.com/originals/dc/45/42/dc4542cb61c32bd20ca933a940f37451.jpg"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Heisenberg</a
+      >
+    </p>
   </footer>
 </template>
+
+<script>
+import { format } from "date-fns";
+
+export default {
+  methods: {
+    currentYear() {
+      return format(new Date(), "y");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$mrWhite: #fff;
+
+.site-footer__link {
+  color: $mrWhite;
+  line-height: 2;
+  text-decoration: none;
+}
+</style>
